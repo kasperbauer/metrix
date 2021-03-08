c0df7bf252d50e63b267706e0c0775c320aadfa6
Christian Kasperbauer <chris@okeydoke.de>
2021-03-08 19:12:43 +0100

    add pulses to voice class

--- lib/voice.lua
+++ lib/voice.lua
@@ -1,12 +1,30 @@
 local Voice = {}
 
-function Voice:new(loop)
-  local t = setmetatable({}, { __index = Voice })
+function Voice:new(args)
+    local t = setmetatable({}, {
+        __index = Voice
+    })
 
-    t.loop = loop or {
+    t.loop = args.loop or {
         start = 1,
         stop = 8
     }
+
+    local steps = {};
+    for i = 1, 8 do
+        steps[i] = {
+            pulses = {}
+        }
+
+        for j = 1, 8 do
+            steps[i].pulses[j] = false
+        end
+
+        for k = 1, i do
+            steps[i].pulses[k] = true
+        end
+    end
+    t.steps = steps
 
     return t
 end
@@ -14,6 +32,18 @@ end
 function Voice:setLoop(start, stop)
     self.loop.start = start or 1
     self.loop.stop = stop or 8
+end
+
+function Voice:setStepLength(step, length)
+    pulses = self.steps[step].pulses
+    for i = 1, 8 do
+        if (i <= length) then
+            pulses[i] = true
+        else
+            pulses[i] = false
+        end
+    end
+    self.steps[step].pulses = pulses
 end
 
 return Voice
