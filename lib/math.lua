8abfbf808b718cd09796317b82c7bb7bc06ca6e1
Christian Kasperbauer <chris@okeydoke.de>
2021-03-16 00:05:03 +0100

    floor random number

--- lib/math.lua
+++ lib/math.lua
@@ -1,4 +1,4 @@
 function math.lowerRandom(min, max, p)
     p = p or 3
-    return min + (max - min) * math.pow(math.random(), p)
+    return math.floor(min + (max - min) * math.pow(math.random(), p))
 end
