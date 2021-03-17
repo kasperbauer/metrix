3a882e2c39d9ce448eee79a11cb8cd2527a73878
Christian Kasperbauer <chris@okeydoke.de>
2021-03-17 19:35:08 +0100

    fix octave ranges

--- metrix.lua
+++ metrix.lua
@@ -73,7 +73,7 @@ function addParams()
         params:add_binary("mute_tr_" .. i, "Mute Track", "toggle", false)
         params:add_separator("Pitch")
         params:add_option("octave_range_tr_" .. i, "Octave Range",
-            {"0 to 4", "1 to 5", "2 to 6", "3 to 7", "4 to 8", "5 to 9"}, 3)
+            {"0 to 3", "1 to 4", "2 to 5", "3 to 6", "4 to 7", "5 to 8", "6 to 9"}, 3)
         params:add_separator("Accumulator")
         params:add_number("transpose_limit_tr_" .. i, "Acc. Limit", 1, 127, 7)
         params:add_option("transpose_trigger_tr_" .. i, "Transpose Trigger", seq:getTransposeTriggers(), 1)
